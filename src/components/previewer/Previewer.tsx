d8f92a3c1eaf6131466269a64c69e5237c375f77
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2023-11-21 18:22:51 -0500

    enabled breaks for editor canvas for smooth typing

--- src/components/previewer/Previewer.tsx
+++ src/components/previewer/Previewer.tsx
@@ -6,13 +6,17 @@ type PreviewerProps = {
 }
 
 export default function Previewer({ text }: PreviewerProps) {
+    marked.use({
+        breaks: true
+    });
+
     const markedText = marked.parse(text);
     // note: only necessary if the form has a submit button
     const cleanText = sanitizeHtml(markedText);
 
     return (
         <fieldset className='flex-1 bg-white ml-2 p-2 rounded-md border-black border'>
-        <legend className="text-2xl font-bold mx-4 text-teal-950">Previewer</legend>
+            <legend className="text-2xl font-bold mx-4 text-teal-950">Previewer</legend>
             <div id="preview"
                 dangerouslySetInnerHTML={{ __html: markedText }}
                 className="prose-sm text-xl prose lg:prose-x prose-slate h-full w-full overflow-y-auto" />
